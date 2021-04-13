7cb6bf0c39f084130d1561b127b76f2f4cd29b93
userJ01 <userJ01@users.noreply.github.com>
2021-04-13 08:54:42 +0300

    last

--- ConsoleApp_Telos_XO/Program.cs
+++ ConsoleApp_Telos_XO/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -33,6 +33,7 @@ namespace ConsoleApp_Telos_XO
             Board2(true);
             do
             {
+                #region Region Print
                 Console.Clear();// whenever loop will be again start then screen will be clear  
                 Console.Write("Player1:");
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -43,6 +44,7 @@ namespace ConsoleApp_Telos_XO
                 Console.Write("O");
                 Console.ForegroundColor = ConsoleColor.Gray;
                 Console.WriteLine("\n");
+
                 if (player % 2 == 0)//checking the chance of the player  
                 {
                     Console.ForegroundColor = ConsoleColor.Cyan;
@@ -55,7 +57,8 @@ namespace ConsoleApp_Telos_XO
                     Console.WriteLine("Player 1 Chance");
                     Console.ForegroundColor = ConsoleColor.Gray;
                 }
-                Console.WriteLine("\n");
+                Console.WriteLine("\n"); 
+                #endregion
 
                 Board2(false);// calling the board Function  
                 if((arr2.Length-1)>9)
@@ -91,8 +94,11 @@ namespace ConsoleApp_Telos_XO
             } while (flag != 1 && flag != -1);// This loof will be run until all cell of the grid is not marked with X and O or some player is not win  
 
             Console.Clear();// clearing the console  
+
             Board2(false);// getting filled board again  
 
+
+            #region Cheked wich user is WIN
             if (flag == 1)// if flag value is 1 then some one has win or means who played marked last time which has win  
             {
                 Console.WriteLine("Player {0} has won", (player % 2) + 1);
@@ -100,23 +106,13 @@ namespace ConsoleApp_Telos_XO
             else// if flag value is -1 the match will be draw and no one is winner  
             {
                 Console.WriteLine("Draw");
-            }
+            } 
+            #endregion
+
             Console.ReadLine();
         }
 
         // Board method which creats board  
-        private static void Board()
-        {
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
-            Console.WriteLine("_____|_____|_____ ");
-            Console.WriteLine("     |     |      ");
-            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
-            Console.WriteLine("     |     |      ");
-        }
         private static void Board2(bool isFirstTime)
         {
             int tempRow = 0;
@@ -186,69 +182,6 @@ namespace ConsoleApp_Telos_XO
         }
 
         #endregion
-
-        //Checking that any player has won or not  
-        private static int CheckWin()
-        {
-            #region Horzontal Winning Condtion
-            //Winning Condition For First Row
-            if (arr2[1] == arr2[2] && arr2[2] == arr2[3])
-            {
-                return 1;
-            }
-            //Winning Condition For Second Row   
-            else if (arr2[4] == arr2[5] && arr2[5] == arr2[6])
-            {
-                return 1;
-            }
-            //Winning Condition For Third Row   
-            else if (arr2[6] == arr2[7] && arr2[7] == arr2[8])
-            {
-                return 1;
-            }
-            #endregion
-
-            #region vertical Winning Condtion
-            //Winning Condition For First Column       
-            else if (arr2[1] == arr2[4] && arr2[4] == arr2[7])
-            {
-                return 1;
-            }
-            //Winning Condition For Second Column  
-            else if (arr2[2] == arr2[5] && arr2[5] == arr2[8])
-            {
-                return 1;
-            }
-            //Winning Condition For Third Column  
-            else if (arr2[3] == arr2[6] && arr2[6] == arr2[9])
-            {
-                return 1;
-            }
-            #endregion
-
-            #region Diagonal Winning Condition
-            else if (arr2[1] == arr2[5] && arr2[5] == arr2[9])
-            {
-                return 1;
-            }
-            else if (arr2[3] == arr2[5] && arr2[5] == arr2[7])
-            {
-                return 1;
-            }
-            #endregion
-
-            #region Checking For Draw
-            // If all the cells or values filled with X or O then any player has won the match  
-            else if (arr2[1] != "1" && arr2[2] != "2" && arr2[3] != "3" && arr2[4] != "4" && arr2[5] != "5" && arr2[6] != "6" && arr2[7] != "7" && arr2[8] != "8" && arr2[9] != "9")
-            {
-                return -1;
-            }
-            #endregion
-            else
-            {
-                return 0;
-            }
-        }
 
         //Checking that any player has won or not  
         private static int CheckWin2()
